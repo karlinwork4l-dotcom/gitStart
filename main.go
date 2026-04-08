c4f55031a605db332efde564467d47f8f87d8476
Alex <karlinwork4l@gmail.com>
2026-04-08 21:59:25 +0300

    Graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println("Hello world")
+	// создаём сервер
+	srv := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(2 * time.Second) // имитация работы
+			w.Write([]byte("Hello"))
+		}),
+	}
+
+	// канал для сигналов ОС
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	// запускаем сервер в горутине
+	go func() {
+		log.Println("Server started on :8080")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// ждём сигнал
+	<-stop
+	log.Println("Shutting down server...")
+
+	// создаём контекст с таймаутом
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// graceful shutdown
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exited properly")
 }
